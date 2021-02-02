b8af5ea3f7de205fa817555badb6237acc64ae2a
fhideous <kul.leshiy@yandex.ru>
2021-02-02 09:42:22 +0300

    playing with main.c && 42docs

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
-#include <stdio.h>
+//#include <stdio.h>
+#include "minilibx_opengl_20191021/mlx.h"
+
+typedef struct  s_data {
+	void        *img;
+	char        *addr;
+	int         bits_per_pixel;
+	int         line_length;
+	int         endian;
+}               t_data;
+
+void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char    *dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int main()
 {
-	printf("Hello, World!\n");
+	void    *mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								 &img.endian);
+	for (int i = 100; i < 1000; i++)
+		my_mlx_pixel_put(&img, i, i, 0x00FF000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
+
+
 	return 0;
 }
